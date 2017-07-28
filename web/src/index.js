44fcd5bfa1c3fa8a24099cc971445f62e39ff8f0
Adlai Holler <guy.on.internet@icloud.com>
2017-07-27 20:17:57 -0700

    Create an network interface, Apollo client and ApolloProvider, use them.

--- web/src/index.js
+++ web/src/index.js
@@ -4,5 +4,22 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// This exposes the core querying/mutation functionality of Apollo.
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+
+// By wrapping React components in this, they can query/mutate the Apollo store.
+import { ApolloProvider } from 'react-apollo'
+
+const apolloClient = new ApolloClient({
+	networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj5lrcpnd0xid0134bh5dnm2n'})
+})
+
+ReactDOM.render((
+	// We wrap App in the ApolloProvider even though it's not using it yet.
+	<ApolloProvider client={apolloClient}>
+		<App />
+	</ApolloProvider>
+	),
+	document.getElementById('root')
+);
 registerServiceWorker();
