4057972a6e73ab576b253c942a5cc6edc95e1b9a
Adlai Holler <guy.on.internet@icloud.com>
2017-11-11 08:53:57 -0800

    Upgrade and fix up more

--- web/src/index.js
+++ web/src/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import TestComponent from './TestComponent.js';
 
 // This exposes the core querying/mutation functionality of Apollo.
-import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import ApolloClient from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 // By wrapping React components in this, they can query/mutate the Apollo store.
 import { ApolloProvider } from 'react-apollo'
 
 const apolloClient = new ApolloClient({
-	networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj5lrcpnd0xid0134bh5dnm2n'})
+	link: createHttpLink({ uri: 'https://api.graph.cool/simple/v1/cj5lrcpnd0xid0134bh5dnm2n' }),
+	cache: new InMemoryCache()
 })
 
 // The root render call.
